dc8cf3a87e358895b9bf402bd765e15fd6ff05f4
lwhstart <lwhstart@163.com>
2020-02-13 20:58:15 +0800

    cs

---
+++ src/main/java/com/wx/controller/DemoController.java
@@ -0,0 +1,23 @@
+package com.wx.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class DemoController {
+	@RequestMapping("getUser")
+	@ResponseBody
+	public List<String> getUser(){
+		System.out.println("输出");
+		List<String> list=new ArrayList<String>();
+		list.add("阿偲");
+		list.add("17");
+		list.add("傻瓜");
+		return list;
+	}
+	
+}
